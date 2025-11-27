83d905f402355f5d276a7a0e965ea4e415d64ea1
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2025-11-27 22:02:18 +0700

    Create index.jsx

---
+++ BuildAReusableFooter/index.jsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+
+  return (<footer>
+
+  <h3>Thar</h3>
+  <ul><li><a href="#">About Us</a></li><li><a href="#">Careers</a></li></ul>
+  <h3>Resources</h3>
+  <ul><li><a href="#">Documentation</a></li><li><a href="#">Blog</a></li></ul>
+
+  <h3>Legal</h3>
+  <ul><li><a href="#">Privacy Policy</a></li><li><a href="#">Terms of service</a></li></ul>
+  <p> © 2025 Thar.All rights reserved.</p>
+  </footer>);
+};
