911e65ed0f73d193bb17acd725a5ea0e8c5b026d
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2026-01-18 13:09:46 +0700

    Create GalacticListBugHunt.jsx

---
+++ CodeSignal/GalacticListBugHunt.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export default function GroceryList() {
+  const [itemName, setItemName] = useState('Milk');
+  const [itemQuantity, setItemQuantity] = useState(2);
+
+  function updateQuantity(newQuantity) {
+    setItemQuantity(newQuantity);
+  }
+
+  return (
+    <div>
+      <p>{itemName}: {itemQuantity} liter(s)</p>
+      <button onClick={() => updateQuantity(5)}>Set 5 liters</button>
+    </div>
+  );
+}
