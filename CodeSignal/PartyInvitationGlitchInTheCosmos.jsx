a97043ce3e1ec44b445e742b10cd575b5ae48f91
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2026-01-25 13:43:10 +0700

    Create PartyInvitationGlitchInTheCosmos.jsx

---
+++ CodeSignal/PartyInvitationGlitchInTheCosmos.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export default function BirthdayGuestList() {
+  const [guest, setGuest] = useState('');
+
+  return (
+    <div>
+      <h1>Welcome to the Virtual Birthday Party in Space, {guest}!</h1>
+      <input
+        type="text"
+        value={guest}
+        onChange={(event) => setGuest(event.target.value)}
+      />
+    </div>
+  );
+}
