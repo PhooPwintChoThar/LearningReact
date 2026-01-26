f7c8215f523d8566a0cf3986afc578b9090d1c27
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2026-01-26 13:37:00 +0700

    Update BorrowABookInTheOnlineLibrarySystem.jsx

--- CodeSignal/BorrowABookInTheOnlineLibrarySystem.jsx
+++ CodeSignal/BorrowABookInTheOnlineLibrarySystem.jsx
@@ -1,1 +1,24 @@
+// TODO: Define a 'Book' component that accepts 'onBorrow' prop and calls it when the borrow button is clicked
+import { useState } from 'react';
 
+function Book({onBorrow}){
+
+    return <button onClick={onBorrow}>borrow</button>;
+    
+}
+
+// TODO: Define a 'Library' component that alerts the name of the borrowed book using the 'handleBorrow' function and renders the 'Book' component
+function Library(){
+    const [book, setBook]=useState()
+    const handleBorrow=()=>{
+        alert(`Borrowed ${book}`)
+    }
+    return <div>
+            <input value={book} onChange={e =>setBook(e.target.value)} type='text'/>
+            <Book onBorrow={handleBorrow}/>
+            </div>
+    
+}
+
+export default Library;
+// TODO: Export the 'Library' component as the default export
