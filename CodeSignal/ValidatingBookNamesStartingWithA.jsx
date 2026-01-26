df0af39906aaa9748acb5c657866a3be2fc63355
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2026-01-26 10:12:41 +0700

    Create ValidatingBookNamesStartingWithA.jsx

---
+++ CodeSignal/ValidatingBookNamesStartingWithA.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function LibraryForm() {
+  const [bookName, setBookName] = useState('');
+
+  const validateBookName = () => {
+    // TODO: Change the code block check if the book name is AstroPhysics
+    // If so, alert 'AstroPhysics book is available. Thank you for your submission!''
+    // Otherwise alert 'Only AstroPhysics book is available for now.'
+    if (bookName=='AstroPhysics') {
+      alert('AstroPhysics book is available. Thank you for your submission!');
+    } else {
+      alert('Only AstroPhysics book is available for now.');
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" value={bookName} onChange={(e) => setBookName(e.target.value)} />
+      <button onClick={validateBookName}>Submit</button>
+    </div>
+  );
+}
+
+export default LibraryForm;
