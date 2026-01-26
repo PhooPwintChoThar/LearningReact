d844f2c8d5cbba980950c0dde3820aa42a20f8b9
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2026-01-26 14:44:48 +0700

    Update BorrowABookInTheOnlineLibrarySystem.jsx

--- CodeSignal/BorrowABookInTheOnlineLibrarySystem.jsx
+++ CodeSignal/BorrowABookInTheOnlineLibrarySystem.jsx
@@ -1,24 +1,33 @@
-// TODO: Define a 'Book' component that accepts 'onBorrow' prop and calls it when the borrow button is clicked
 import { useState } from 'react';
 
-function Book({onBorrow}){
-
-    return <button onClick={onBorrow}>borrow</button>;
-    
+// Child component
+function Book({ title, onBorrow }) {
+  return (
+    <button onClick={() => onBorrow(title)}>
+      Borrow
+    </button>
+  );
 }
 
-// TODO: Define a 'Library' component that alerts the name of the borrowed book using the 'handleBorrow' function and renders the 'Book' component
-function Library(){
-    const [book, setBook]=useState()
-    const handleBorrow=()=>{
-        alert(`Borrowed ${book}`)
-    }
-    return <div>
-            <input value={book} onChange={e =>setBook(e.target.value)} type='text'/>
-            <Book onBorrow={handleBorrow}/>
-            </div>
-    
+// Parent component
+function Library() {
+  const [book, setBook] = useState('');
+
+  const handleBorrow = (borrowedTitle) => {
+    alert(`Borrowed ${borrowedTitle}`);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={book}
+        onChange={(e) => setBook(e.target.value)}
+        placeholder="Enter book title"
+      />
+      <Book title={book} onBorrow={handleBorrow} />
+    </div>
+  );
 }
 
 export default Library;
-// TODO: Export the 'Library' component as the default export
