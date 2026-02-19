47da8b065fa6fe87a5d5f8db353a589ea69fb5fc
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2026-02-19 14:31:45 +0700

    Create StellarCodeNavigation:RectifyingTheDataFetchAnomaly.jsx

---
+++ CodeSignal/StellarCodeNavigation:RectifyingTheDataFetchAnomaly.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useFetchAuthor = authorId => {
+  const [author, setAuthor] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://api-regional.codesignalcontent.com/posting-application-2/users/${authorId}`)
+      .then(response => response.json())
+      .then(data => {
+        setAuthor(data);
+        setLoading(false);
+      });
+  }, [authorId]);
+
+  return { author, loading };
+};
+
+export default useFetchAuthor;
